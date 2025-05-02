d5f7cd5ec7c2d02af7c695bb0da5b4b5481afabd
Piter N. <gabrielliendo1257>
2025-05-01 19:42:41 -0500

    noice plugin configured

--- plugins.lua
+++ plugins.lua
@@ -49,8 +49,8 @@ return {
                         'prettier', -- prettier formatter
                         -- 'stylua', -- lua formatter
                         -- 'isort', -- python formatter
-                        'black', -- python formatter
-                        'mypy', -- python linter
+                        'black',    -- python formatter
+                        'mypy',     -- python linter
                         'eslint_d', -- js linter
                         'pyright',
                         -- 'lua-language-server',
@@ -272,6 +272,60 @@ return {
             }
             return opts
         end,
+    },
+
+    {
+        'folke/noice.nvim',
+        event = 'VeryLazy',
+        dependencies = {
+            { 'MunifTanjim/nui.nvim', lazy = true },
+            {
+                'rcarriga/nvim-notify',
+                lazy = true,
+                event = 'VeryLazy',
+                keys = {
+                    {
+                        '<leader>un',
+                        function()
+                            require('notify').dismiss { silent = true, pending = true }
+                        end,
+                        desc = 'Delete all Notifications',
+                    },
+                },
+                opts = {
+                    timeout = 3000,
+                    max_height = function()
+                        return math.floor(vim.o.lines * 0.75)
+                    end,
+                    max_width = function()
+                        return math.floor(vim.o.columns * 0.75)
+                    end,
+                },
+            },
+        },
+        opts = {
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+                inc_rename = false,
+                lsp_doc_border = true,
+            },
+            lsp = {
+                progress = {
+                    enabled = true,
+                    format = 'lsp_progress',
+                    format_done = 'lsp_progress_done',
+                    throttle = 1000 / 30,
+                    view = 'mini',
+                },
+                override = {
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true,
+                },
+            },
+        },
     },
 
     {
